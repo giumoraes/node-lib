8478b1f444b0413f48deaee59c162162a2820d62
Giuliano Pascoeto <giiu.moraes@gmail.com>
2023-11-19 12:33:57 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo!'));
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
 
-console.log(chalk.blue.bgWhite.bold('Alura'));
+// async/await
 
-console.log('olá mundo');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro)
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+
+// }
+
+
+pegaArquivo('./arquivos/texto.md');
+// pegaArquivo('./arquivos/');
