b0e5c0d3cb11d156eb271478295a9eaac612e59e
Giuliano Pascoeto <giiu.moraes@gmail.com>
2023-11-19 21:13:33 -0300

    aula 5

--- src/cli.js
+++ src/cli.js
@@ -1,25 +1,46 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import pegaArquivo from './index.js';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('lista de links'), resultado);
+async function imprimeLista(valida, resultado, arquivo = '') {
+
+    if (valida) {
+        console.log(
+            chalk.yellow('lista validada'),
+            chalk.black.bgGreen(arquivo),
+            await listaValidada(resultado));    
+    } else {
+        console.log(
+            chalk.yellow('lista de links de'),
+            chalk.black.bgGreen(arquivo),
+            resultado);
+    }
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+    
+    try{
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log('arquivo ou diretório não existe');
+            return;
+        }
+    }
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        imprimeLista(resultado);
-
+        imprimeLista(valida, resultado);
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimeLista(lista);
+            imprimeLista(valida, lista, nomeDeArquivo);
         })
     }
 }
